2741c1028059332aae92434c787abb6d9b0a9b55
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2023-10-27 11:59:52 +0000

    Added folder problems

---
+++ Problems/A2_S24_20220360_3.cpp
@@ -0,0 +1,40 @@
+// File: A2_S24_20220360_3.cpp
+// Purpose: Solving the third problem
+// Author: Dr. Mohammed El-Ramly
+// Section: S24
+// ID: 20220360
+// TA: To be determined
+// Date: 27 Oct 2023
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<string> split(string target, string delimeter){
+    vector <string> v;
+    string s = "";
+    for (int i = 0; i < target.size(); ++i) {
+        if(target[i] == delimeter[0]){
+            v.push_back(s);
+            s = "";
+        }
+        else
+            s+=target[i];
+    }
+    if(s != "")
+        v.push_back(s);
+    return v;
+}
+
+int main(){
+    vector <string> vec;
+    string target, delimeter;
+    getline(cin,target);
+    getline(cin,delimeter);
+    vec = split(target, delimeter);
+    for(auto i : vec){
+        cout << i << " ";
+    }
+}
