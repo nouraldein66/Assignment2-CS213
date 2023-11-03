65108c5e8c7e7f0e7bbe486a5dfc3873f9d3437c
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2023-11-03 10:43:08 +0200

    Update BigReal.h
    - Added setNum
    - Added getNum
    - Added compare_fraction

--- BigReal/BigReal.h
+++ BigReal/BigReal.h
@@ -8,10 +8,15 @@ class BigReal {
 private:
     string s;
 public:
-//    bool isValid();
+    bool isValid();
     bool isNeg();
     bool operator==(const BigReal &a) const;
-    BigReal operator+(BigReal& a);
+    BigReal operator+(BigReal &a);
+    BigReal operator-(BigReal &a);
     bool operator<(BigReal &another_real);
     bool operator>(BigReal &anotherreal);
+    void setNum(string s);
+    string getNum() const;
 };
+
+bool compare_fraction(string s1, string s2);
