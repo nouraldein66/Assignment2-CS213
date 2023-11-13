e254e44ba8c923d0382748030c910950ab4a9ddb
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2023-11-13 17:25:45 +0200

    Update Machine.h

--- VoleMachineSimulator/Machine.h
+++ VoleMachineSimulator/Machine.h
@@ -7,13 +7,23 @@ using namespace std;
 
 const int SIZE = 256;
 
+struct Instruction {
+    int op;
+    int register1;
+    int register2;
+    int register3;
+    int memory;
+};
+
 class Machine {
 private:
+    Instruction ins;
     int IR;
     int PC;
 public:
     void getIRVAl();
     void getPCVAl();
 };
+
 
 #endif //VOLEMACHINESIMULATOR_MACHINE_H
