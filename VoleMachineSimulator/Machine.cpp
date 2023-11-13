402ca623447dec3a5514f2dd3642f86d2724c463
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2023-11-13 18:55:53 +0200

    Update Machine.cpp

--- VoleMachineSimulator/Machine.cpp
+++ VoleMachineSimulator/Machine.cpp
@@ -1,1 +1,9 @@
 #include "Machine.h"
+
+void Machine::jump(int reg_address, int mem_address) {
+    if (reg[reg_address] == reg[0]) {
+        PC = mem_address;
+    } else {
+        ++++PC;
+    }
+}
