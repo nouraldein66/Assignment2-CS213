d83f2bb20b9fd987f48ea48aa67b55474edbaef0
ahmed-alaa-22 <147052378+ahmed-alaa-22@users.noreply.github.com>
2023-11-14 22:20:14 +0200

    Update Machine.cpp

--- VoleMachineSimulator/Machine.cpp
+++ VoleMachineSimulator/Machine.cpp
@@ -15,16 +15,16 @@ void Machine::copyRegisters(int register1, int register2) {
 }
 
 //add the bit-patterns in R1 and R2 as two's complement
-void Machine::ADDition(int targetRegister, int register1, int register2) { 
+void Machine::ADDition(int targetRegister, int register1, int register2) {
     deque<int> B1 = binaryRepresentation(reg[register1]);
     deque<int> B2 = binaryRepresentation(reg[register2]);
     int twosComplement1 = 0, twosComplement2 = 0;
-    
+
     if(B1[0] == 1)
         twosComplement1 = -(pow(2,B1.size()-1));
     if(B2[0] == 1)
         twosComplement2 = -(pow(2,B2.size()-1));
-    
+
     for (int i = 1; i < B1.size(); ++i) {
         if(B1[i] == 1)
             twosComplement1+=(pow(2,B1.size()-i-1));
@@ -46,4 +46,23 @@ deque<int> Machine::binaryRepresentation(int n){
         n /= 2;
     }
     return dq;
+}
+
+void Machine::Load(int op, int R, int value) {
+   if(R >= REG_SIZE || R < 0)
+   {
+       cout<<"Invalid instruction\n";
+       return;
+   }
+    reg[R] = value;
+}
+
+void Machine::Load(int R, int ad) {
+    if(R >= REG_SIZE || R < 0 || ad > SIZE || ad < 0)
+    {
+        cout<<"Invalid instruction\n";
+        return;
+    }
+
+reg[R] = mem[ad];
 }
