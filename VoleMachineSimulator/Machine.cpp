c67beee7d209255d62a6cb0620e8e6ff7b56930a
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2023-11-14 11:41:44 +0200

    Update Machine.cpp

--- VoleMachineSimulator/Machine.cpp
+++ VoleMachineSimulator/Machine.cpp
@@ -7,3 +7,43 @@ void Machine::jump(int reg_address, int mem_address) {
         ++++PC;
     }
 }
+
+// Instruction 4, which copies the bit patterns found in register 1 to register 2
+void Machine::copyRegisters(int register1, int register2) {
+    reg[register2] = reg[register1];
+    PC+=2;
+}
+
+//add the bit-patterns in R1 and R2 as two's complement
+void Machine::ADDition(int targetRegister, int register1, int register2) { 
+    deque<int> B1 = binaryRepresentation(reg[register1]);
+    deque<int> B2 = binaryRepresentation(reg[register2]);
+    int twosComplement1 = 0, twosComplement2 = 0;
+    
+    if(B1[0] == 1)
+        twosComplement1 = -(pow(2,B1.size()-1));
+    if(B2[0] == 1)
+        twosComplement2 = -(pow(2,B2.size()-1));
+    
+    for (int i = 1; i < B1.size(); ++i) {
+        if(B1[i] == 1)
+            twosComplement1+=(pow(2,B1.size()-i-1));
+    }
+    for (int i = 1; i < B2.size(); ++i) {
+        if(B2[i] == 1)
+            twosComplement2+=(pow(2,B2.size()-i-1));
+    }
+
+    reg[targetRegister] = twosComplement1 + twosComplement2;
+
+    PC+=2;
+}
+
+deque<int> Machine::binaryRepresentation(int n){
+    deque <int> dq;
+    while (n != 0) {
+        dq.push_front(n%2);
+        n /= 2;
+    }
+    return dq;
+}
