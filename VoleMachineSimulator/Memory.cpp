6515ec69f0ecd9b91bdcfca43b9080f3473c7d97
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2023-11-15 09:20:52 +0200

    Update Memory.cpp

--- VoleMachineSimulator/Memory.cpp
+++ VoleMachineSimulator/Memory.cpp
@@ -1,6 +1,10 @@
 #include "Memory.h"
 
 void Memory::store(int val, int ad) {
+    if (ad > 255 || ad < 0) {
+        cout << "Invalid Instruction." << endl;
+        return;
+    }
     if (ad == 0) {
         cout << hex << val << endl;
         return;
